ea83e2b6b688fd45387035a45859637ed14cc87d
Aditya Upadhyay <upadhyayaditya469@gmail.com>
2026-07-06 09:38:22 +0530

    Refactor selection sort into a class method

--- Sorting/Selection-Sort/Solution.cpp
+++ Sorting/Selection-Sort/Solution.cpp
@@ -1,36 +1,20 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 // Selection Sort
 // Time Complexity: O(n²)
 // Space Complexity: O(1)
 
-void selectionSort(vector<int>& arr) {
-    int n = arr.size();
-
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-
-        // Find the index of the smallest element
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex])
-                minIndex = j;
+class Solution {
+public:
+    vector<int> selectionSort(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            int minIndex=i;
+            for(int j=i;j<n;j++){
+                if(nums[minIndex]>nums[j]){
+                    minIndex=j;
+                }
+            }
+            swap(nums[i] ,nums[minIndex]);
         }
-
-        // Place the smallest element at its correct position
-        swap(arr[i], arr[minIndex]);
+        return nums;
     }
-}
-
-int main() {
-    vector<int> arr = {64, 25, 12, 22, 11};
-
-    selectionSort(arr);
-
-    cout << "Sorted Array: ";
-    for (int num : arr)
-        cout << num << " ";
-
-    return 0;
-}
+};

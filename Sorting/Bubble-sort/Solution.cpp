319dacc000af20a34c1e984f78f1c5fb2aff819a
Aditya Upadhyay <upadhyayaditya469@gmail.com>
2026-07-06 09:31:55 +0530

    Convert bubbleSort to class method in Solution
    Refactor bubble sort implementation into a class method.

--- Sorting/Bubble-sort/Solution.cpp
+++ Sorting/Bubble-sort/Solution.cpp
@@ -1,17 +1,14 @@
-void bubbleSort(vector<int>& arr) {
-    int n = arr.size();
-
-    for (int i = 0; i < n - 1; i++) {
-        bool swapped = false;
-
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
-                swapped = true;
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0l;i<n-1;i++){
+            for(int j=0;j<n-1;j++){
+                if(nums[j]>nums[j+1]){
+                    swap(nums[j],nums[j+1]);
+                }
             }
         }
-
-        if (!swapped)
-            break;
+        return nums;
     }
-}
+};
